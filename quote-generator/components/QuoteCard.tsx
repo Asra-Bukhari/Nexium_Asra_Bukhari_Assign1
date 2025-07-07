ffc293e194aa8bedb278620bb7daa010dfe1c8ae
Asra-Bukhari <asrabukhari2003@gmail.com>
2025-07-07 06:22:30 -0700

    fix: mobile view

--- quote-generator/components/QuoteCard.tsx
+++ quote-generator/components/QuoteCard.tsx
@@ -124,7 +124,7 @@ export default function QuoteCard() {
       </motion.div>
 
       {/* Video */}
-      <div className="w-full md:w-[450px] md:max-h-[720px] -mt-4 md:mt-0">
+      <div className="w-full md:w-[450px] md:max-h-[720px] -mt-8 md:mt-0">
         <video
           autoPlay
           muted
